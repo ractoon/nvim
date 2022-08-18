91d2151af394015deea02682619da99589a5aec9
ractoon <ractoon@gmail.com>
2022-08-18 15:52:15 -0600

    Fix color theme

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,7 +1,7 @@
-vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_transparent = true
+vim.opt.background = "dark"
 
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
--- vim.cmd[[colorscheme tokyonight]]
+vim.cmd[[colorscheme tokyonight]]
