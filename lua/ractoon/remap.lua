192a2cab3051e581bfa690ed7837d46199415e73
ractoon <ractoon@gmail.com>
2022-08-21 13:56:12 -0600

    Jump, jump around

--- lua/ractoon/remap.lua
+++ lua/ractoon/remap.lua
@@ -2,16 +2,20 @@ local nnoremap = require("ractoon.keymap").nnoremap
 
 -- netrw
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+nnoremap("<C-h>", ":bprev<cr>")
+nnoremap("<C-l>", ":bnext<cr>")
+
 
 -- Telescope
 nnoremap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
 nnoremap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 nnoremap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+nnoremap("<leader>fs", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>")
 nnoremap("<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
 nnoremap("<D-j>", "<cmd>:terminal<cr>")
 
 -- NERDTree
-nnoremap ("<leader>n", "<cmd>:NERDTreeFocus<cr>")
-nnoremap ("<C-n>", "<cmd>:NERDTree<cr>")
-nnoremap ("<C-t>", "<cmd>:NERDTreeToggle<cr>")
-nnoremap ("<C-f>", "<cmd>:NERDTreeFind<cr>")
+-- nnoremap ("<leader>n", "<cmd>:NERDTreeFocus<cr>")
+-- nnoremap ("<C-n>", "<cmd>:NERDTree<cr>")
+-- nnoremap ("<C-t>", "<cmd>:NERDTreeToggle<cr>")
+-- nnoremap ("<C-f>", "<cmd>:NERDTreeFind<cr>")
